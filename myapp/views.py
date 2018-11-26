83000c9900b56ae9d68384ea8eeb64d6106c3577
Yu Ishiguro <yuuis.egr@gmail.com>
2018-11-26 22:17:25 +0900

    add vires

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import sys, codecs
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
-# Create your views here.
+from django.http.response import HttpResponse
+from django.shortcuts import (render, redirect,)
+from django import forms
+from myapp.models import Posting
+from .forms import PostingForm
+
+def index(request):
+  form = PostingForm(request.POST or None)
+  if request.method == 'POST':
+    if form.is_valid():
+      form.save()
+      return redirect('myapp:index')
+  new_text = Posting.objects.all().order_by('-id')
+  contexts = {
+    'form':form,
+    'new_text':new_text,
+  }
+  return render(request, 'index.html', contexts)
